b57295c2e4c20dc5754c5983a138fd09211024ad
YellowFlash1901 <vardaansathe@gmail.com>
2024-09-26 11:48:00 +0100

    now can add new task to task list

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
 import "./styles.css" 
- 
- 
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { useState } from "react";
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+
+
 export default function App(){
+  const[newitem, setnewitem] = useState("");
+  const[todos, settodos] = useState([]);
+  function handlesumbit(e){
+    e.preventDefault();
+    settodos((currenttodos)=>{
+      return[
+        ...currenttodos,{title : newitem}
+      ]
+    })
+  }
   return (
     <div className="App">
-    <div className="Side-bar-parent"><div className="Side-bar">User Name</div><div className="Side-bar">User Name</div><div className="Side-bar">User Name</div><div className="Side-bar">User Name</div><div className="Side-bar">User Name</div><div className="Side-bar">User Name</div></div> 
-    {/* <div className="Mainwindow-parent">
-    HI  
-    </div> */}
+    <div className="Side-bar-parent"><div className="Side-bar">User Name</div></div> 
+    <div className="Mainwindow-parent">
+    <div id="todolabel">
+      Todo:
     </div>
+    <form className="todoinput" htmlFor ="Input" onSubmit={handlesumbit}
+            value={newitem}
+        onChange={(e) => setnewitem(e.target.value)}>
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      </form>     
+    </div>
+    <label id="tasklist">
+      Task List:  
+    </label>
+    {todos.map(todo => {
+      return     <FormControlLabel required control={<Checkbox />} label={todo.title} />
+
+    })}
+
+      </div>
 
     //to make user name 
   )
